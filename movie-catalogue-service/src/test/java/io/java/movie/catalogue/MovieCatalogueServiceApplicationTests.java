582f34f24dea664f535d77ab927595c1b8836e3b
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-16 17:59:07 +0530

    movie catalogue data and info services for microservices

---
+++ movie-catalogue-service/src/test/java/io/java/movie/catalogue/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.movie.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
