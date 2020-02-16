74b16367002d3f054e403a8bd83a3a76629ef553
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-16 20:34:09 +0530

    Added code to use webclient in stead of rest template

--- movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieCatalogueResource.java
@@ -11,27 +11,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogueResource {
 	
+	//@Autowired
+	//RestTemplate restTemplate;
+	
 	@Autowired
-	RestTemplate restTemplate;
+	WebClient.Builder builder;
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogueItem> getCatalogue(@PathVariable("userId")String userId){
 		
 		List<Rating> ratings = Arrays.asList(
-			new Rating("1234","2"),
+			new Rating("1234","3"),
 			new Rating("5678","4")
 				);
 		List<CatalogueItem> catItems = new ArrayList<CatalogueItem>();
-		for(Rating rating : ratings) {
-			Movie movie = restTemplate.getForObject("http://localhost:8282/movies/"+rating.getMovieId(), Movie.class);
+		
+		for (Rating rating : ratings) {
+			Movie movie = builder.build()
+					             .get()
+					             .uri("http://localhost:8282/movies/" + rating.getMovieId())
+					             .retrieve()
+					             .bodyToMono( Movie.class)
+					             .block();
 			CatalogueItem ci = new CatalogueItem(movie.getName(), "test", rating.getRating());
 			catItems.add(ci);
 		}
+		
+		//use this code with restTemplate object
+		//for(Rating rating : ratings) {
+		//	Movie movie = restTemplate.getForObject("http://localhost:8282/movies/"+rating.getMovieId(), Movie.class);
+		//	CatalogueItem ci = new CatalogueItem(movie.getName(), "test", rating.getRating());
+		//	catItems.add(ci);
+		//}
 	return catItems;
 	}
 
