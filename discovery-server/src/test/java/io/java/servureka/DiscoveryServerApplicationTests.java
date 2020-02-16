8ae606ef84ac142a92774a1c273e3c81d8a9c9e9
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-16 22:45:29 +0530

    adding eureka server project

---
+++ discovery-server/src/test/java/io/java/servureka/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.servureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
