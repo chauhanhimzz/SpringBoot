d80842c40725ef8fa696d554937af65e7e9ba1b4
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-16 20:04:28 +0530

    added RestTemplate to call movie info service

--- movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieCatalogueResource.java
@@ -1,11 +1,15 @@
 package io.java.movie.catalogue.resource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/catalog")
@@ -13,9 +17,20 @@ public class MovieCatalogueResource {
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogueItem> getCatalogue(@PathVariable("userId")String userId){
-		return Collections.singletonList(
-				new CatalogueItem("transformers","test","2")
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		List<Rating> ratings = Arrays.asList(
+			new Rating("1234","2"),
+			new Rating("5678","4")
 				);
+		List<CatalogueItem> catItems = new ArrayList<CatalogueItem>();
+		for(Rating rating : ratings) {
+			Movie movie = restTemplate.getForObject("http://localhost:8282/movies/"+rating.getMovieId(), Movie.class);
+			CatalogueItem ci = new CatalogueItem(movie.getName(), "test", rating.getRating());
+			catItems.add(ci);
+		}
+	return catItems;
 	}
 
 }
