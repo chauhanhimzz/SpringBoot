a4f03f37b0759c8734dc62ddf480bc3e3e2397e4
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-19 00:13:10 +0530

    microservices-added HystrixProperty in UserRatingService

--- movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/UserRatingService.java
+++ movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/UserRatingService.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
@@ -14,7 +14,14 @@ public class UserRatingService {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod="getFallbackUserRating")
+	@HystrixCommand(fallbackMethod = "getFallbackUserRating", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000") }
+
+	)
+	
 	public UserRating getUserRating(String userId) {
 		//----using service discovery
 		UserRating userRating = restTemplate.getForObject("http://movie-data-service/data/users/"+userId,
