f410f9fff90ec3bcee429e33ae7449d6c0dad6a0
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-19 23:54:26 +0530

    spring boot configuration

---
+++ spring-boot-config/src/main/java/io/java/conf/GreetingController.java
@@ -0,0 +1,30 @@
+package io.java.conf;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	@Value("${my.greetings: defalut value}")
+	private String greetingMsg;
+	
+	@Value("${app.description}")
+	private String description ;
+	
+	@Value("${my.list.val}")
+	private List<String> alp;
+	
+	@Value("#{${db.conn.val}}")
+	public Map<String, String> mymap;
+	
+	@GetMapping("/greeting")
+	public String greeting() {
+		return greetingMsg + description + alp + mymap;
+	}
+
+}
---
+++ spring-boot-config/src/main/java/io/java/conf/SpringBootConfigApplication.java
@@ -0,0 +1,13 @@
+package io.java.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigApplication.class, args);
+	}
+
+}
