ec32a8b834175173d3fd99e8f0ff387646875f52
chauhanhimzz <himanshuchauhan1986@gmail.com>
2020-02-19 10:38:21 +0530

    Microservices-added circuit breaker properties

--- movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieInfoService.java
+++ movie-catalogue-service/src/main/java/io/java/movie/catalogue/resource/MovieInfoService.java
@@ -1,12 +1,14 @@
 package io.java.movie.catalogue.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
+@EnableHystrix
 public class MovieInfoService {
 	
 	@Autowired
